eacff54fc26baf128c1fb7aaf4417797035c6ed0
knokko <knokogator@hotmail.com>
2025-01-31 15:04:27 +0100

    Fix epic cut ordering bug

--- include/reconfiguration/job_orderings.hpp
+++ include/reconfiguration/job_orderings.hpp
@@ -73,10 +73,11 @@ namespace NP::Reconfiguration {
 
 			std::vector<size_t> current_node_indices{cuts[cut_index].node_index};
 			std::vector<size_t> next_node_indices;
-
+			Index_set has_seen_node;
 			while (!current_node_indices.empty()) {
 				for (const size_t node_index : current_node_indices) {
 					bool has_dispatched = false;
+
 					size_t edge_index;
 					{
 						const Rating_edge dummy_search_edge(node_index, node_index, 0);
@@ -88,14 +89,16 @@ namespace NP::Reconfiguration {
 
 					while (edge_index < graph.edges.size() && graph.edges[edge_index].get_child_node_index() == node_index) {
 						const auto &edge = graph.edges[edge_index];
+						edge_index += 1;
+						if (has_seen_node.contains(edge.get_parent_node_index())) continue;
+						has_seen_node.add(edge.get_parent_node_index());
 						next_node_indices.push_back(edge.get_parent_node_index());
 						if (!has_dispatched && node_index != cuts[cut_index].node_index) job_orderings.advance();
 						has_dispatched = true;
 						job_orderings.mark_dispatchable(edge.get_taken_job_index());
-						edge_index += 1;
 					}
 				}
-				std::swap(current_node_indices, next_node_indices);
+				current_node_indices.swap(next_node_indices);
 				next_node_indices.clear();
 			}
 
